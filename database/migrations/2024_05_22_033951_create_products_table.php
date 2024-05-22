e70a4382f7b7ab00cc02c3394ae80e3a817506a7
Zarkprogame <zarkprogame@gmail.com>
2024-05-21 23:00:09 -0500

    Correccion en la migracion para productos

--- database/migrations/2024_05_22_033951_create_products_table.php
+++ database/migrations/2024_05_22_033951_create_products_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name',80)->nullable();
+            $table->string('name', 80)->nullable();
             $table->integer('price')->nullable();
             $table->integer('stock')->nullable();
-            $table->integer('category_id')->nullable();
             $table->timestamps();
+            $table->foreignId('category_id')->nullable()->references('id')->on('categories');
         });
     }
 
