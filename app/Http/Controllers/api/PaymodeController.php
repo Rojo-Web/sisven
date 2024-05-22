ea77e6bf48b212accb56e6081385a5c2f5c1c361
Zarkprogame <zarkprogame@gmail.com>
2024-05-21 19:40:35 -0500

    Controlador para Paymode Terminado

--- app/Http/Controllers/api/PaymodeController.php
+++ app/Http/Controllers/api/PaymodeController.php
@@ -17,51 +17,36 @@ class PaymodeController extends Controller
 
     public function store(Request $request)
     {
-        $validate = Validator::make($request->all(), [
-            'name' => ['required'],
-            'observation' => ['required'],
-        ]);
-
-        if ($validate->fails()) {
-            return response()->json([
-                'msg' => 'Error al validar la informacion',
-                'statusCode' => 400
-            ]);
-        }
-
         $paymode = new Paymode();
         $paymode->id = $request->id;
         $paymode->name = $request->name;
         $paymode->observation = $request->observation;
         $paymode->save();
-        return json_encode(['paymode' => $paymode, 'success' => true]);
+        return json_encode(['paymode' => $paymode]);
     }
 
     public function show(string $id)
     {
-        $paymodes = Paymode::all();
-        return json_encode(['paymodes' => $paymodes]);
+        $paymode = Paymode::find($id);
+        return json_encode(['paymode' => $paymode]);
     }
 
     public function update(Request $request, string $id)
     {
         $paymode = Paymode::find($id);
-        if (is_null($paymode)) {
-            return abort(404);
-        }
+        $paymode->id = $request->id;
         $paymode->name = $request->name;
         $paymode->observation = $request->observation;
         $paymode->save();
-        return json_encode(['paymode' => $paymode, 'success' => true]);
+        return json_encode(['paymode' => $paymode]);
     }
 
     public function destroy(string $id)
     {
         $paymode = Paymode::find($id);
-        if (is_null($paymode)) {
-            return abort(404);
-        }
         $paymode->delete();
-        return json_encode(['paymode' => $paymode, 'success' => true]);
+
+        $paymodes = Paymode::all();
+        return json_encode(['paymodes' => $paymodes, 'success' => true]);
     }
 }
