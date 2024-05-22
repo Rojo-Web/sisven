d2539b92f16013aac0589dad2c41860c03288c02
Zarkprogame <zarkprogame@gmail.com>
2024-05-21 20:03:48 -0500

    Controlador para Category Terminado

--- app/Http/Controllers/api/CategoryController.php
+++ app/Http/Controllers/api/CategoryController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use App\Models\categorie;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
 {
@@ -17,54 +16,36 @@ class CategoryController extends Controller
 
     public function store(Request $request)
     {
-        $validate = Validator::make($request->all(), [
-            'name' => ['required'],
-            'description' => ['required'],
-        ]);
-
-        if ($validate->fails()) {
-            return response()->json([
-                'msg' => 'Error al Validar la Informacion',
-                'statusCode' => 400
-            ]);
-        }
-        $categorie = new categorie();
-        $categorie->name = $request->name;
-        $categorie->description = $request->description;
-        $categorie->id = $request->id;
-        $categorie->save();
-        return json_encode(['categorie' => $categorie, 'success' => true]);
+        $category = new categorie();
+        $category->name = $request->name;
+        $category->description = $request->description;
+        $category->id = $request->id;
+        $category->save();
+        return json_encode(['category' => $category]);
     }
 
     public function show(string $id)
     {
-        $categorie = categorie::find($id);
-        if (is_null($categorie)) {
-            return abort(404);
-        }
-        return json_encode(['categorie' => $categorie]);
+        $category = categorie::find($id);
+        return json_encode(['category' => $category]);
     }
 
     public function update(Request $request, string $id)
     {
-        $categorie = categorie::find($id);
-        if (is_null($categorie)) {
-            return abort(404);
-        }
-        $categorie->name = $request->name;
-        $categorie->description = $request->description;
-        $categorie->save();
-        return json_encode(['categorie' => $categorie, 'success' => true]);
+        $category = categorie::find($id);
+        $category->name = $request->name;
+        $category->description = $request->description;
+        $category->id = $request->id;
+        $category->save();
+        return json_encode(['category' => $category]);
     }
 
     public function destroy(string $id)
     {
-        $categorie = categorie::find($id);
-        if (is_null($categorie)) {
-            return abort(404);
-        }
-        $categorie = categorie::find($id);
-        $categorie->delete();
-        return json_encode(['categorie' => $categorie, 'success' => true]);
+        $category = categorie::find($id);
+        $category->delete();
+
+        $categories = categorie::all();
+        return json_encode(['categories' => $categories, 'success' => true]);
     }
 }
